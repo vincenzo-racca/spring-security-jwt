b2a254b0348a6b68ef13090fdfaf50f6b71b103d
vraca <vracca@sysmanagement.it>
2022-08-30 09:57:30 +0200

    update Spring Boot version to 2.7.3

--- src/main/java/com/vincenzoracca/springsecurityjwt/security/SecurityConfig.java
+++ src/main/java/com/vincenzoracca/springsecurityjwt/security/SecurityConfig.java
@@ -3,50 +3,49 @@ package com.vincenzoracca.springsecurityjwt.security;
 import com.vincenzoracca.springsecurityjwt.security.filter.CustomAuthenticationFilter;
 import com.vincenzoracca.springsecurityjwt.security.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Created by Vincenzo Racca
+ *
+ * Spring Security Config with version before 2.7
  */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 
-    private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    //we want to check users from db
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        http
+                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/login/**").permitAll()
-                .and()
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/users/**").hasAuthority("ROLE_ADMIN")
-                .and()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .addFilter(new CustomAuthenticationFilter(super.authenticationManagerBean()))
-                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers(HttpMethod.POST, "/login/**").permitAll()
+                        .antMatchers(HttpMethod.POST, "/users/**").hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilter(new CustomAuthenticationFilter(authenticationManager))
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .headers().cacheControl();
 
-        http.headers().cacheControl();
+        return http.build();
     }
 
 }
